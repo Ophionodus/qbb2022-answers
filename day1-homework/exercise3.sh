dc61b21ea9d03e40edb8e4b585036f0020d069f8
evanschlesinger <eschles4@jh.edu>
2022-08-30 09:29:37 -0400

    exercises from hw day1

---
+++ day1-homework/exercise3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#USAGE: bash exercise3.sh input_VCF
+
+vcffile=$1
+
+awk '/^#/{next} {print $1,$2-1, $2}' $vcffile > variants.bed
+sort -k1,1 -k2,2n ~/data/bed_files/genes.bed > genes.sorted.bed
+bedtools closest -a variants.bed -b genes.sorted.bed
