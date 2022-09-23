f75c5bdb0bd1e664811750c3511ba2569f8eb9e8
evanschlesinger <eschles4@jh.edu>
2022-09-23 09:27:39 -0400

    I can't even turn in my homework; I am literally incapable, this is shameful.

---
+++ day2-homework/day2hwscript.py
@@ -0,0 +1,15 @@
+#Create an analysis script that will import your VCF parser, load both the 1KGP genomes (random_snippet.vcf) and dbSNP (dbSNP_snippet.vcf) VCF files, and then annotate the 1KGP variants with the IDs from the dbSNP file, when appropriate. Your script should include the following features:
+
+# Fully commented
+# Loads the records from dbSNP_snippet.vcf and random_snippet.vcf
+# Creates a dictionary of positions and IDs from the dbSNP file
+# Replaces the ID in each record from random_snippet.vcf with the correct label, if it exists, from your dbSNP dictionary
+# Finds and reports the number of random_snippet.vcf records that do not have a corresponding ID in your dbSNP ID dictionary
+#
+
+#!/usr/bin/env python3
+import sys
+
+def relabeler(fname1, fname2):
+    #create a dictionary of positions and IDs from the dbSNP file
+    
